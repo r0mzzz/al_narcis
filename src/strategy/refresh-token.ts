bb71e620a5413992f36471a0226e535a17fcfe82
Ramazan Haciyev <ramazanh@goldenpay.az>
2022-12-22 16:21:33 +0400

    changes in user service and controller

--- src/strategy/refresh-token.ts
+++ src/strategy/refresh-token.ts
@@ -18,7 +18,7 @@ export class RefreshTokenStrategy extends PassportStrategy(
   }
 
   validate(req: Request, payload: any) {
-    const refreshToken = req.get('Authorization').replace('Bearer', '').trim();
-    return { ...payload, refreshToken };
+    const refresh_token = req.get('Authorization').replace('Bearer', '').trim();
+    return { ...payload, refresh_token };
   }
 }
