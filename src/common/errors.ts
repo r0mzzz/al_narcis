bff0b350be95330f40225197a965c5114af628b2
Ramazan Haciyev <ramazanh@goldenpay.az>
2023-02-06 13:47:54 +0400

    password validation pattern added

--- src/common/errors.ts
+++ src/common/errors.ts
@@ -5,4 +5,6 @@ export const AppError = {
   ACCESS_DENIED: 'Access denied',
   USERNAME_EXISTS: 'Username already taken',
   ID_DOESNT_EXISTS: 'User with this id doesnt exists',
+  PASSWORD_PATTERN_MATCH:
+    'password must contain at least 1 upper case, 1 lower case, numeric, and special character',
 };
