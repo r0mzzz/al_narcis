025d146f4a32c749b8d9418b94865113f9efe4f5
Ramazan Haciyev <ramazanh@goldenpay.az>
2023-02-09 13:32:52 +0400

    user.controller testing

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './user.controller';
 import { UsersService } from './user.service';
 import { CreateUserDto } from './dto/user.dto';
+import { UpdateUserDto } from './dto/updateuser.dto';
 
 describe('UserController', () => {
   let controller: UsersController;
@@ -12,6 +13,13 @@ describe('UserController', () => {
       return {
         ...dto,
       };
+    }),
+    update: jest.fn().mockImplementation((id, dto) => ({
+      id,
+      ...dto,
+    })),
+    findAll: jest.fn(() => {
+      return [];
     }),
   };
 
@@ -42,6 +50,30 @@ describe('UserController', () => {
       };
       expect(controller.create(userCreateDto)).toEqual(userCreateDto);
       expect(mockUserService.create).toHaveBeenCalledWith(userCreateDto);
+    });
+  });
+
+  describe('updateUser', () => {
+    it('should update a user', () => {
+      const userUpdateDto: UpdateUserDto = {
+        first_name: 'Test',
+        last_name: 'Test',
+        username: 'Test',
+      };
+      expect(controller.update('1', userUpdateDto)).toEqual({
+        first_name: 'Test',
+        last_name: 'Test',
+        username: 'Test',
+        id: '1',
+      });
+      expect(mockUserService.update).toHaveBeenCalled();
+    });
+  });
+
+  describe('findAllUsers', () => {
+    it('should return all users', () => {
+      expect(controller.findAll()).toEqual({});
+      expect(mockUserService.findAll).toHaveBeenCalled();
     });
   });
 });
