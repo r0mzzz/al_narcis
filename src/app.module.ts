6146c4a61b44bf95e93f4795707e364446c13924
r0mzzz <perfectingthevoid@gmail.com>
2025-09-11 14:30:30 +0400

    changed tokens

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,6 @@ import { TodoModule } from './modules/todo/todo.module';
 import { ConfigModule } from '@nestjs/config';
 import configurations from './configuration';
 import { AuthModule } from './modules/auth/auth.module';
-import * as path from 'path';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { FileModule } from './modules/file/file.module';
 
 @Module({
   imports: [
@@ -15,15 +12,10 @@ import { FileModule } from './modules/file/file.module';
       load: [configurations],
     }),
     MongooseModule.forRoot(
-      'mongodb+srv://admin:admin12345@cluster0.gbqtupu.mongodb.net/?retryWrites=true&w=majority',
+      'mongodb://localhost:27017',
     ),
     TodoModule,
     AuthModule,
-    ServeStaticModule.forRoot({
-      rootPath: path.resolve(__dirname, 'images'),
-      serveRoot: '/images',
-    }),
-    FileModule,
   ],
 })
 export class AppModule {}
