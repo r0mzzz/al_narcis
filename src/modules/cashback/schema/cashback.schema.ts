c33bb1cbc7336ec255a5369a8c264a16ed6c4bf9
r0mzzz <perfectingthevoid@gmail.com>
2025-10-13 21:53:54 +0400

    added orders implementation

--- src/modules/cashback/schema/cashback.schema.ts
+++ src/modules/cashback/schema/cashback.schema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
+import { OrderStatus } from '../../order/order-status.enum';
 
 export type CashbackDocument = Cashback & Document;
 
@@ -7,6 +8,17 @@ export enum CashbackType {
   REFERRAL = 'REFERRAL',
   PURCHASE = 'PURCHASE',
   BONUS = 'BONUS',
+}
+
+export class ProductOrderDto {
+  @Prop()
+  productId: string;
+  @Prop()
+  productName: string;
+  @Prop()
+  variant: string;
+  @Prop()
+  count: number;
 }
 
 @Schema({ timestamps: true })
@@ -26,14 +38,41 @@ export class Cashback {
   @Prop({ type: Types.ObjectId, ref: 'User', required: false, default: null })
   from_user_id?: Types.ObjectId | null;
 
-  // Add any other fields you want to persist from CreateOrderDto, with correct types
+  @Prop({ type: Object })
+  status: { code: number; message: string };
+
+  @Prop()
+  merchantName: string;
+
   @Prop()
   amount: number;
 
   @Prop()
+  checkCount: number;
+
+  @Prop()
   paymentDate: string;
 
-  // ...add more fields as needed
+  @Prop()
+  cardNumber: string;
+
+  @Prop()
+  language: string;
+
+  @Prop()
+  description: string;
+
+  @Prop()
+  rrn: string;
+
+  @Prop()
+  phoneNumber: string;
+
+  @Prop({ enum: OrderStatus })
+  orderStatus: OrderStatus;
+
+  @Prop({ type: [Object] })
+  products: ProductOrderDto[];
 }
 
 export const CashbackSchema = SchemaFactory.createForClass(Cashback);
