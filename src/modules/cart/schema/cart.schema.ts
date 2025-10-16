9375ebc971989871343006c13f2fc5e830567c0e
r0mzzz <perfectingthevoid@gmail.com>
2025-10-16 14:36:08 +0400

    cart implementation

--- src/modules/cart/schema/cart.schema.ts
+++ src/modules/cart/schema/cart.schema.ts
@@ -1,14 +1,57 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
-import { CartItemDto } from '../dto/cart-item.dto';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+
+@Schema({ _id: false })
+export class ProductVariant {
+  @Prop({ required: true })
+  capacity: number;
+
+  @Prop({ required: true })
+  price: number;
+
+  @Prop({ required: false })
+  _id?: string;
+
+  @Prop({ required: false, default: 1 })
+  count?: number;
+}
+
+export const ProductVariantSchema =
+  SchemaFactory.createForClass(ProductVariant);
+
+@Schema({ _id: false })
+export class CartProduct {
+  @Prop({ required: true })
+  productId: string;
+
+  @Prop({ type: [ProductVariantSchema], default: [] })
+  variants: ProductVariant[];
+
+  @Prop({ required: false })
+  productName?: string;
+  @Prop({ required: false })
+  productDesc?: string;
+  @Prop({ required: false })
+  productImage?: string;
+  @Prop({ required: false })
+  productType?: string;
+  @Prop({ required: false })
+  category?: string[];
+  @Prop({ required: false })
+  gender?: string;
+  @Prop({ required: false })
+  brand?: string;
+}
+
+export const CartProductSchema = SchemaFactory.createForClass(CartProduct);
 
 @Schema({ timestamps: true })
 export class Cart extends Document {
   @Prop({ required: true })
   user_id: string;
 
-  @Prop({ type: Array, default: [] })
-  products: CartItemDto[];
+  @Prop({ type: [CartProductSchema], default: [] })
+  products: CartProduct[];
 }
 
 export const CartSchema = SchemaFactory.createForClass(Cart);
