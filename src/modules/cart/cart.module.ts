4537bc80f71b9556f7b057dbf34aa2afd8343f14
r0mzzz <perfectingthevoid@gmail.com>
2025-10-17 12:20:32 +0400

    cart implementation

--- src/modules/cart/cart.module.ts
+++ src/modules/cart/cart.module.ts
@@ -3,12 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Cart, CartSchema } from './schema/cart.schema';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
+import { MinioModule } from '../../services/minio.module';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Cart.name, schema: CartSchema }])],
+  imports: [
+    MongooseModule.forFeature([{ name: Cart.name, schema: CartSchema }]),
+    MinioModule,
+  ],
   controllers: [CartController],
   providers: [CartService],
   exports: [CartService],
 })
 export class CartModule {}
-
