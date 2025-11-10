d600b8e6e8b46ad1d2f58aae33abad8168b080f2
r0mzzz <perfectingthevoid@gmail.com>
2025-11-10 21:56:54 +0400

    subcategories  implementation

--- src/modules/banner/banner.module.ts
+++ src/modules/banner/banner.module.ts
@@ -4,11 +4,13 @@ import { Banner, BannerSchema } from './schema/banner.schema';
 import { BannerService } from './banner.service';
 import { BannerController } from './banner.controller';
 import { MinioModule } from '../../services/minio.module';
+import { JwtSharedModule } from '../../services/jwt-shared.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Banner.name, schema: BannerSchema }]),
     MinioModule,
+    JwtSharedModule,
   ],
   controllers: [BannerController],
   providers: [BannerService],
